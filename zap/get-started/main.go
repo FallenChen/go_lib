379d9b93e83eb931e1e07f04ffed73cd6655cd2b
GarryChen <chenymj23@gmail.com>
2022-07-11 17:09:13 +0800

    zap

---
+++ zap/get-started/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"go.uber.org/zap"
+	"time"
+)
+
+func main() {
+	logger := zap.NewExample()
+	defer logger.Sync()
+
+	url := "http://example.org/api"
+	logger.Info("failed to fetch URL",
+		// Structured context as strongly typed Field values.
+		zap.String("url", url),
+		zap.Int("attempt", 3),
+		zap.Duration("backoff", time.Second),
+	)
+
+	sugar := logger.Sugar()
+	sugar.Infow("failed to fetch URL",
+		// Structured context as loosely typed key-value pairs.
+		"url", url,
+		"attempt", 3,
+		"backoff", time.Second,
+	)
+	sugar.Infof("Failed to fetch URL: %s", url)
+}
---
+++ zap/std/redirect/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"go.uber.org/zap"
+	"log"
+)
+
+func main() {
+	logger := zap.NewExample()
+	defer logger.Sync()
+
+	undo := zap.RedirectStdLog(logger)
+	log.Print("redirected standard library")
+	undo()
+
+	log.Print("restored standard library")
+}
